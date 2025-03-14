96b4b9b1768299220c44240f4d77b43cee637a19
bombonchik <sashayuriychuk672@gmail.com>
2025-03-14 17:28:21 +0100

    Initialize NestJS project

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    {
+        ignores: ['eslint.config.mjs', 'dist/**', 'node_modules/**'],
+    },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    eslintPluginPrettierRecommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
+            ecmaVersion: 5,
+            sourceType: 'module',
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+    {
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-floating-promises': 'warn',
+            '@typescript-eslint/no-unsafe-argument': 'warn',
+
+            // 🔥 Force 4-space indentation
+            //indent: ['error', 4],
+            intend: 'off',
+            // 🔥 Ensure Prettier follows 4-space indent
+            'prettier/prettier': ['error', { tabWidth: 4, useTabs: false }],
+        },
+    },
+);
