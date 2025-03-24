b2ff68e3f3f077d000024da4583abafe6219b2a6
bombonchik <sashayuriychuk672@gmail.com>
2025-03-24 23:23:39 +0100

    Add Auth decorator and SupabaseAuthProvider

--- src/auth/interfaces/auth-provider.interface.ts
+++ src/auth/interfaces/auth-provider.interface.ts
@@ -2,11 +2,5 @@ import { AuthUserDto} from '@/auth/dto/auth-user.dto';
 
 export const AUTH_PROVIDER = Symbol('AUTH_PROVIDER');
 export interface AuthProvider {
-    /**
-     * Validates the given token.
-     * Returns an object containing user information if valid, or null otherwise.
-     */
-    //validateToken(token: string): Promise<{ providerUid: string } | undefined>;
-    //getUserByProviderUid(providerUid: string): Promise<AuthUser | undefined>;
     getAuthUserByJwt(jwt: string): Promise<AuthUserDto | undefined>;
 }
