7208f253b0dc61135029f191a92f9bcccb24d14e
bombonchik <sashayuriychuk672@gmail.com>
2025-04-10 16:40:07 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
+//import './instrument';
 import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './all-exceptions.filter';
+import { UserInterceptor } from '@/user/interceptors/user.interceptor';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-
+    const configService = app.get(ConfigService);
     const { httpAdapter } = app.get(HttpAdapterHost);
-    app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
-
+    app.useGlobalFilters(new AllExceptionsFilter(configService, httpAdapter));
+    app.useGlobalInterceptors(app.get(UserInterceptor));
     app.enableCors(); // For now api is open to all origins (to everyone)
     app.setGlobalPrefix('v1');
 
-    await app.listen(process.env.PORT ?? 3000);
+    await app.listen(configService.get<string>('PORT') ?? 3000);
 }
 bootstrap();
