0445190871bdaf36edae4bc57fc91c008cb9fdd7
bombonchik <sashayuriychuk672@gmail.com>
2025-04-10 16:09:55 +0200

    Update all-exceptions.filter.ts

--- src/all-exceptions.filter.ts
+++ src/all-exceptions.filter.ts
@@ -8,7 +8,12 @@ import {
 import { BaseExceptionFilter } from '@nestjs/core';
 import { Request, Response } from 'express';
 import { AppLoggerService } from './app-logger/app-logger.service';
-import { PrismaClientValidationError } from '@prisma/client/runtime/library';
+import {
+    PrismaClientKnownRequestError,
+    PrismaClientValidationError,
+} from '@prisma/client/runtime/library';
+import { ConfigService } from '@nestjs/config';
+import { SentryExceptionCaptured } from '@sentry/nestjs';
 
 type MyResponseObj = {
     statusCode: number;
@@ -21,12 +26,17 @@ type MyResponseObj = {
 export class AllExceptionsFilter extends BaseExceptionFilter {
     private readonly logger: AppLoggerService;
     private readonly isDevelopment: boolean;
-    constructor(applicationRef?: HttpServer) {
+    constructor(
+        private readonly configService: ConfigService,
+        applicationRef?: HttpServer,
+    ) {
         super(applicationRef);
         this.logger = new AppLoggerService(AllExceptionsFilter.name);
-        this.isDevelopment = process.env.NODE_ENV !== 'production';
+        this.isDevelopment =
+            this.configService.get<string>('NODE_ENV') !== 'production';
     }
 
+    //@SentryExceptionCaptured()
     catch(exception: unknown, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
@@ -42,6 +52,20 @@ export class AllExceptionsFilter extends BaseExceptionFilter {
             responseMessage = this.isDevelopment
                 ? exception.message.replace(/\n/g, '')
                 : 'Invalid request data';
+        } else if (exception instanceof PrismaClientKnownRequestError) {
+            this.logger.error(
+                `Prisma error caught in exception filter, code: ${exception.code}, message: ${exception.message}`,
+            );
+            if (exception.code === 'P2002') {
+                statusCode = HttpStatus.CONFLICT;
+                responseMessage = 'Duplicate data not allowed'; // or a generic message
+                (
+                    exception as { suppressSuperCatch?: boolean }
+                ).suppressSuperCatch = true;
+            } else {
+                statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+                responseMessage = 'Invalid request data';
+            }
         }
 
         const myResponse: MyResponseObj = {
@@ -61,6 +85,10 @@ export class AllExceptionsFilter extends BaseExceptionFilter {
                 AllExceptionsFilter.name,
             );
         }
-        super.catch(exception, host);
+        if (
+            !(exception as { suppressSuperCatch?: boolean }).suppressSuperCatch
+        ) {
+            super.catch(exception, host);
+        }
     }
 }
