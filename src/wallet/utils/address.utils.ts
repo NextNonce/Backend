980d6c2f440866a21ee026981613105b1016ea51
bombonchik <sashayuriychuk672@gmail.com>
2025-04-13 10:29:07 +0200

    Convert AddressUtils to class

--- src/wallet/utils/address.utils.ts
+++ src/wallet/utils/address.utils.ts
@@ -6,58 +6,62 @@ import { throwLogged } from '@/common/helpers/error.helper';
 import { BadRequestException } from '@nestjs/common';
 import { AppLoggerService } from '@/app-logger/app-logger.service';
 
-const logger = new AppLoggerService('AddressUtils');
+export class AddressUtils {
+    private readonly logger: AppLoggerService;
 
-export function isStarknetAddress(address: string): boolean {
-    if (address.length !== STARKNET_ADDRESS_LENGTH) return false;
-    try {
-        validateAndParseAddress(address);
-        return true;
-    } catch {
-        return false;
+    constructor() {
+        this.logger = new AppLoggerService('AddressUtils');
     }
-}
 
-export function isEVMAddress(address: string): boolean {
-    return isAddress(address);
-}
-
-export function getChainType(address: string): ChainType | undefined {
-    if (isEVMAddress(address)) return ChainType.EVM;
-    if (isStarknetAddress(address)) return ChainType.CAIROVM;
-
-    return undefined;
-}
-
-export function normalizeEVMAddress(address: string): string {
-    try {
-        return getAddress(address.toLowerCase());
-    } catch {
-        logger.warn(`Failed to get checksum EVM address: ${address}`);
-        throwLogged(new BadRequestException('Invalid address'));
+    public isStarknetAddress(address: string): boolean {
+        if (address.length !== STARKNET_ADDRESS_LENGTH) return false;
+        try {
+            validateAndParseAddress(address.toLowerCase());
+            return true;
+        } catch {
+            return false;
+        }
     }
-}
 
-export function normalizeStarknetAddress(address: string): string {
-    try {
-        return getChecksumAddress(address.toLowerCase());
-    } catch {
-        logger.warn(`Failed to get checksum Starknet address: ${address}`);
-        throwLogged(new BadRequestException('Invalid address'));
+    public isEVMAddress(address: string): boolean {
+        return isAddress(address.toLowerCase());
     }
-}
 
-export function normalizeWalletAddress(
-    address: string,
-    chain: ChainType,
-): string {
-    switch (chain) {
-        case ChainType.EVM:
-            return normalizeEVMAddress(address);
-        case ChainType.CAIROVM:
-            return normalizeStarknetAddress(address);
-        default:
-            logger.error(`Unhandled ChainType`);
-            throwLogged(new BadRequestException('Invalid chain type'));
+    public getChainType(address: string): ChainType | undefined {
+        if (this.isEVMAddress(address)) return ChainType.EVM;
+        this.logger.debug(`Invalid EVM address: ${address}`);
+        if (this.isStarknetAddress(address)) return ChainType.CAIROVM;
+        this.logger.debug(`Invalid Starknet address: ${address}`);
+        return undefined;
+    }
+
+    public normalizeEVMAddress(address: string): string {
+        try {
+            return getAddress(address.toLowerCase());
+        } catch {
+            this.logger.warn(`Failed to get checksum EVM address: ${address}`);
+            throwLogged(new BadRequestException('Invalid address'));
+        }
+    }
+
+    public normalizeStarknetAddress(address: string): string {
+        try {
+            return getChecksumAddress(address.toLowerCase());
+        } catch {
+            this.logger.warn(`Failed to get checksum Starknet address: ${address}`);
+            throwLogged(new BadRequestException('Invalid address'));
+        }
+    }
+
+    public normalizeWalletAddress(address: string, chain: ChainType): string {
+        switch (chain) {
+            case ChainType.EVM:
+                return this.normalizeEVMAddress(address);
+            case ChainType.CAIROVM:
+                return this.normalizeStarknetAddress(address);
+            default:
+                this.logger.error(`Unhandled ChainType`);
+                throwLogged(new BadRequestException('Invalid chain type'));
+        }
     }
 }
