7be35fbc64ec79b69669590e25175fe0776cae4f
bombonchik <sashayuriychuk672@gmail.com>
2025-06-22 11:33:56 +0200

    UnifiedTokenDto: Add API documentation

--- src/token/dto/unified-token.dto.ts
+++ src/token/dto/unified-token.dto.ts
@@ -1,11 +1,29 @@
 import { TokenMetadataDto } from '@/token/dto/token-metadata.dto';
 import { TokenDto } from '@/token/dto/token.dto';
 import { TokenPriceDto } from '@/token/dto/token-price.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UnifiedTokenDto {
+    @ApiProperty({
+        description: 'Discriminator for the single token type.',
+        enum: ['single'], // Explicitly list the enum value
+        example: 'single',
+    })
     type: 'unified';
+    @ApiProperty({
+        type: [TokenDto],
+        description: 'List of tokens that are unified.',
+    })
     tokens: TokenDto[];
+    @ApiProperty({
+        type: TokenMetadataDto,
+        description: 'Metadata of the unified token.',
+    })
     tokenMetadata: TokenMetadataDto;
+    @ApiProperty({
+        type: TokenPriceDto,
+        description: 'Price information for the unified token.',
+    })
     tokenPrice: TokenPriceDto;
 
     constructor(
