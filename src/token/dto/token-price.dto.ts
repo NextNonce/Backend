da05e1a70d6c7be12936b82aeac33b7f7d129a8a
bombonchik <sashayuriychuk672@gmail.com>
2025-06-22 11:33:34 +0200

    TokenPriceDto: Add API documentation

--- src/token/dto/token-price.dto.ts
+++ src/token/dto/token-price.dto.ts
@@ -1,13 +1,34 @@
 import { Decimal } from '@prisma/client/runtime/library';
 import { TokenPrice } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class TokenPriceDto {
-    priceUsd: Decimal;
+    @ApiProperty({
+        type: 'string',
+        format: 'decimal',
+        description: 'The price of the token in quotes (e.g., USD)',
+        example: '123.456',
+    })
+    priceQuote: Decimal;
+    @ApiProperty({
+        type: 'string',
+        format: 'decimal',
+        description: 'The change in price, if available, in quotes (e.g., USD)',
+        example: '1.23',
+        required: false,
+        nullable: true,
+    })
     change: Decimal | null;
+    @ApiProperty({
+        type: 'string',
+        format: 'date-time',
+        description: 'The timestamp when the price was last updated',
+        example: '2024-12-01T12:00:00Z',
+    })
     timestamp: Date;
 
     constructor(tokenPrice: TokenPrice, change: Decimal | null = null) {
-        this.priceUsd = tokenPrice.priceUsd;
+        this.priceQuote = tokenPrice.priceUsd;
         this.change = change;
         this.timestamp = tokenPrice.timestamp;
     }
