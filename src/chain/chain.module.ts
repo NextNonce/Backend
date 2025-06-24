3cf47ca7083b1ff173c14f38e5207bf422d6a4ad
bombonchik <sashayuriychuk672@gmail.com>
2025-06-24 14:46:41 +0200

    Chain: Add OkxDexChainMapper

--- src/chain/chain.module.ts
+++ src/chain/chain.module.ts
@@ -4,6 +4,10 @@ import {
     DUNE_CHAIN_MAPPER,
     DuneChainMapper,
 } from '@/chain/mappers/dune-chain.mapper';
+import {
+    OKX_DEX_CHAIN_MAPPER,
+    OkxDexChainMapper,
+} from '@/chain/mappers/okx-dex-chain.mapper';
 
 @Module({
     providers: [
@@ -11,8 +15,12 @@ import {
             provide: DUNE_CHAIN_MAPPER,
             useClass: DuneChainMapper,
         },
+        {
+            provide: OKX_DEX_CHAIN_MAPPER,
+            useClass: OkxDexChainMapper,
+        },
         ChainService,
     ],
-    exports: [DUNE_CHAIN_MAPPER, ChainService],
+    exports: [DUNE_CHAIN_MAPPER, OKX_DEX_CHAIN_MAPPER, ChainService],
 })
 export class ChainModule {}
