c1b931a6c5506c131f58bcff50359485c7cc737d
bombonchik <sashayuriychuk672@gmail.com>
2025-06-24 14:12:54 +0200

    CacheProvider: Add mget, mset

--- src/cache/interfaces/cache-provider.interface.ts
+++ src/cache/interfaces/cache-provider.interface.ts
@@ -1,3 +1,5 @@
+import { CacheMSetItem } from '@/cache/interfaces/cache-mset-item.interface';
+
 export const CACHE_PROVIDER = Symbol('CACHE_PROVIDER');
 
 // Interface defining the core operations the underlying provider must support
@@ -8,4 +10,18 @@ export interface CacheProvider {
     getWithMetadata<T>(
         key: string,
     ): Promise<{ value: T; ageInSeconds: number } | undefined>;
+    /**
+     * Retrieves multiple keys from the cache in a single operation.
+     * @param keys An array of keys to retrieve.
+     * @returns A promise that resolves to an array of values. The order of
+     * values matches the order of the keys. If a key is not found, the
+     * corresponding array element will be undefined.
+     */
+    mget<T>(keys: string[]): Promise<(T | undefined)[]>;
+
+    /**
+     * Sets multiple key-value pairs in the cache in a single operation.
+     * @param items An array of objects, each with a key, value, and optional TTL.
+     */
+    mset<T>(items: CacheMSetItem<T>[]): Promise<void>;
 }
