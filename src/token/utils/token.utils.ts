c595e5e033532aba772cb2b2bb90f1655e9108a1
bombonchik <sashayuriychuk672@gmail.com>
2025-06-26 14:44:52 +0200

    TokenUtils: Add toTokenWithMetadata, getKeyFromTokenIdentifier

--- src/token/utils/token.utils.ts
+++ src/token/utils/token.utils.ts
@@ -1,4 +1,8 @@
-import { TokenWithDetails, TokenWithMetadata } from '@/token/types/token.types';
+import {
+    TokenIdentifier,
+    TokenWithDetails,
+    TokenWithMetadata,
+} from '@/token/types/token.types';
 
 export function toTokenWithMetadata(
     tokenWithDetails: TokenWithDetails,
@@ -9,4 +13,20 @@ export function toTokenWithMetadata(
         address: tokenWithDetails.address,
         tokenMetadata: tokenWithDetails.tokenMetadata,
     };
+}
+
+export function getKeyFromTokenIdentifier(
+    tokenIdentifier: TokenIdentifier,
+): string {
+    return getKeyFromChainNameAndAddress(
+        tokenIdentifier.chainName,
+        tokenIdentifier.address,
+    );
+}
+
+export function getKeyFromChainNameAndAddress(
+    chainName: string,
+    address: string,
+): string {
+    return `${chainName}-${address}`;
 }
