d50921506751c5e1d9f0cf84bd7d4cd81448dec3
bombonchik <sashayuriychuk672@gmail.com>
2025-06-26 14:47:41 +0200

    UnifiedTokenDto: Add balances: BalanceDto[]

--- src/token/dto/unified-token.dto.ts
+++ src/token/dto/unified-token.dto.ts
@@ -2,19 +2,27 @@ import { TokenMetadataDto } from '@/token/dto/token-metadata.dto';
 import { TokenDto } from '@/token/dto/token.dto';
 import { TokenPriceDto } from '@/token/dto/token-price.dto';
 import { ApiProperty } from '@nestjs/swagger';
+import { BalanceDto } from '@/balance/dto/balance.dto';
 
 export class UnifiedTokenDto {
     @ApiProperty({
-        description: 'Discriminator for the single token type.',
-        enum: ['single'], // Explicitly list the enum value
-        example: 'single',
+        description: 'Discriminator for the unified token type.',
+        enum: ['unified'], // Explicitly list the enum value
+        example: 'unified',
     })
     type: 'unified';
     @ApiProperty({
         type: [TokenDto],
         description: 'List of tokens that are unified.',
+        isArray: true,
     })
     tokens: TokenDto[];
+    @ApiProperty({
+        type: [BalanceDto],
+        description: 'Balances of individual tokens across different chains.',
+        isArray: true,
+    })
+    balances: BalanceDto[];
     @ApiProperty({
         type: TokenMetadataDto,
         description: 'Metadata of the unified token.',
@@ -28,10 +36,13 @@ export class UnifiedTokenDto {
 
     constructor(
         tokens: TokenDto[],
+        balances: BalanceDto[],
         tokenMetadata: TokenMetadataDto,
         tokenPrice: TokenPriceDto,
     ) {
+        this.type = 'unified';
         this.tokens = tokens;
+        this.balances = balances;
         this.tokenMetadata = tokenMetadata;
         this.tokenPrice = tokenPrice;
     }
