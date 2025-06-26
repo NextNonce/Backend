85c53c7fae9585f7f0b68e3665ae1e61438be4e9
bombonchik <sashayuriychuk672@gmail.com>
2025-06-26 15:05:59 +0200

    TokenModule: Export TokenService

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TokenService } from './token.service';
+import { ChainModule } from '@/chain/chain.module';
 @Module({
+    imports: [ChainModule],
     providers: [TokenService],
+    exports: [TokenService],
 })
 export class TokenModule {}
