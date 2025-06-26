74d0b9ab8f8e3b6f1705001e34bc1391c96bf791
bombonchik <sashayuriychuk672@gmail.com>
2025-06-26 16:28:13 +0200

    BalanceModule: Import necessary modules

--- src/balance/balance.module.ts
+++ src/balance/balance.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BalanceService } from './balance.service';
+import { MetadataModule } from '@/metadata/metadata.module';
+import { ChainModule } from '@/chain/chain.module';
+import { OkxDexBalanceProvider } from '@/balance/providers/okx-dex-balance.provider';
+import { PriceModule } from '@/price/price.module';
+import { TokenModule } from '@/token/token.module';
 
 @Module({
     providers: [BalanceService],
+    imports: [MetadataModule, ChainModule, PriceModule, TokenModule],
     exports: [BalanceService],
 })
 export class BalanceModule {}
