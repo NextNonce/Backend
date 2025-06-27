07f11ec98132af48fc55760370075dfde0b8bee5
bombonchik <sashayuriychuk672@gmail.com>
2025-06-27 09:55:00 +0200

    WalletModule: Import BalanceModule

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
+import { BalanceModule } from '@/balance/balance.module';
+import { WalletTypeUtils } from '@/wallet/utils/wallet-type.utils';
+import { AddressUtils } from '@/wallet/utils/address.utils';
 
 @Module({
+    imports: [BalanceModule],
     controllers: [WalletController],
-    providers: [WalletService],
+    providers: [WalletService, WalletTypeUtils, AddressUtils],
     exports: [WalletService],
 })
 export class WalletModule {}
