94e00bf45f01b0371afb3c90619b584239cb726f
bombonchik <sashayuriychuk672@gmail.com>
2025-07-08 16:46:03 +0200

    WalletModule: Add WALLET_TYPE_PROVIDER

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -4,11 +4,21 @@ import { WalletController } from './wallet.controller';
 import { BalanceModule } from '@/balance/balance.module';
 import { WalletTypeUtils } from '@/wallet/utils/wallet-type.utils';
 import { AddressUtils } from '@/wallet/utils/address.utils';
+import { WALLET_TYPE_PROVIDER } from '@/wallet/interfaces/wallet-type-provider.interface';
+import { AlchemyWalletTypeProvider } from '@/wallet/providers/alchemy-wallet-type.provider';
 
 @Module({
     imports: [BalanceModule],
     controllers: [WalletController],
-    providers: [WalletService, WalletTypeUtils, AddressUtils],
+    providers: [
+        WalletService,
+        {
+            provide: WALLET_TYPE_PROVIDER,
+            useClass: AlchemyWalletTypeProvider,
+        },
+        WalletTypeUtils,
+        AddressUtils,
+    ],
     exports: [WalletService],
 })
 export class WalletModule {}
